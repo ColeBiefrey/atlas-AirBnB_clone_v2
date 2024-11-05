abdff2e8c4670932c8fe61d35036e6f2ab741334
Cole Kelsey <Cole.Kelsey@atlasschool.com>
2024-11-05 05:54:52 -0600

    file updates + 7

--- models/state.py
+++ models/state.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
 """ holds class State"""
-import models
-from models.base_model import BaseModel, Base
-from models.city import City
 from os import getenv
-import sqlalchemy
-from sqlalchemy import Column, String, ForeignKey
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import sessionmaker, scoped_session
+from sqlalchemy import (create_engine)
+from sqlalchemy.ext.declarative import declarative_base
+from models.base_model import Base
+from models.state import State
+from models.city import City
+from models.user import User
+from models.place import Place
+from models.review import Review
+from models.amenity import Amenity
 
 
 class State(BaseModel, Base):
@@ -32,3 +36,66 @@ class State(BaseModel, Base):
                 if city.state_id == self.id:
                     city_list.append(city)
             return city_list
+
+
+class DBStorage:
+    """ create tables in environmental """
+    __engine = None
+    __session = None
+
+    def __init__(self):
+        user = getenv("HBNB_MYSQL_USER")
+        passwd = getenv("HBNB_MYSQL_PWD")
+        db = getenv("HBNB_MYSQL_DB")
+        host = getenv("HBNB_MYSQL_HOST")
+        env = getenv("HBNB_ENV")
+
+        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'
+                                      .format(user, passwd, host, db),
+                                      pool_pre_ping=True)
+
+        if env == "test":
+            Base.metadata.drop_all(self.__engine)
+
+    def all(self, cls=None):
+        """ returns a dictionary """
+        dic = {}
+        if cls:
+            if type(cls) is str:
+                cls = eval(cls)
+            query = self.__session.query(cls)
+            for elem in query:
+                key = "{}.{}".format(type(elem).__name__, elem.id)
+                dic[key] = elem
+        else:
+            lista = [State, City, User, Place, Review, Amenity]
+            for clase in lista:
+                query = self.__session.query(clase)
+                for elem in query:
+                    key = "{}.{}".format(type(elem).__name__, elem.id)
+                    dic[key] = elem
+        return (dic)
+
+    def new(self, obj):
+        """ adds new element to table """
+        self.__session.add(obj)
+
+    def save(self):
+        """ save changes """
+        self.__session.commit()
+
+    def delete(self, obj=None):
+        """ deletes element from table """"
+        if obj:
+            self.session.delete(obj)
+
+    def reload(self):
+        """ configuration """
+        Base.metadata.create_all(self.__engine)
+        sec = sessionmaker(bind=self.__engine, expire_on_commit=False)
+        Session = scoped_session(sec)
+        self.__session = Session()
+
+    def close(self):
+        """ calls for remove """
+        self.__session.close()
