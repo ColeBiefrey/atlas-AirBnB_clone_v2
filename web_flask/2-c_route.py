4ed7390225da9a043d8c2317907a88fab68a7b35
Cole Kelsey <Cole.Kelsey@atlasschool.com>
2024-11-05 05:15:28 -0600

    pep8 + 2

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -12,5 +12,17 @@ def hello_hbnb():
     return 'Hello HBNB!'
 
 
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ called function using hbnb route """
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    """ called function using text route """
+    return 'C %s' % text.replace('_', ' ')
+
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
